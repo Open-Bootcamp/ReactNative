abe932c0b5b8a15f454c78be408b573b65d3b17b
ramzeta13 <ramiroperezsanz1313@gmail.com>
2022-02-15 16:17:40 +0100

    second commit

--- App.js
+++ App.js
@@ -1,15 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, ScrollView, StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import { firebaseConfig } from './firebase-config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import {initializeApp} from 'firebase/app';
+//navegaror entre pantallas
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+function Login(){
+  //hooks userState
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  //Utilizamos Firebase y lo configuramos con el archivo externo
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleCreateAccount = () =>{
+    //Crear una cuenta
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredencial) => {
+      console.log('Account created!!');
+      const user = userCredencial.user;
+      console.log(user);
+    }).catch(error =>{
+      console.log(error);
+      Alert.alert(error.message);
+    })
+
+  }
+  
+  const navigation = useNavigation();
+
+  const handleSignIn = () =>{
+    //Iniciar sesion
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log('Signed in!')
+      const user = userCredential.user;
+      console.log(user)
+      navigation.navigate('Home');
+    })
+    .catch(error => {
+      console.log(error)
+      Alert.alert(error.message)
+    })
+  }
+
+
+
+  return(
+    <View>
+       <View>
+          <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
+          <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="ramiro@prueba.com" />
+        </View>
+        <View>
+          <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
+          <TextInput onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="password" secureTextEntry={true}/>
+        </View>
+          <TouchableOpacity onPress={handleSignIn} style={[styles.button, {backgroundColor: '#00CFDB90'}]}>
+            <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {backgroundColor: '#1792F090'}]}>
+            <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+          </TouchableOpacity>
+    </View>
+  )
+}
+
+function Home(){
+
+  return(
+    <View>
+      <Text>Bienvenido a tu perfil</Text>
     </View>
   );
 }
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Login" component={Login}/>
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +100,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    marginVertical: 30
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20
+  },
+  button: {
+    width: 350,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderColor: '#fff',
+    backgroundColor:'#fff',
+    borderWidth: 1,
+  }
+})
+
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyCWMfZkbHJ1e-iT8MdyzMcejkjWFUVil8M",
+    authDomain: "curso-react-99238.firebaseapp.com",
+    projectId: "curso-react-99238",
+    storageBucket: "curso-react-99238.appspot.com",
+    messagingSenderId: "330335975525",
+    appId: "1:330335975525:web:6ec9660c09c9f96d8d6e8c"
+  };
